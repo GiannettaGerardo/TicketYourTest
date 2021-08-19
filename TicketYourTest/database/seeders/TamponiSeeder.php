dea8f944f25c74bf6766cc02ed0ea4b7ab0361fa
CatignanoFrancesco <catignanof@gmail.com>
2021-08-19 18:22:40 +0200

    Aggiornato il seeder TamponiSeeder
    Il seeder TamponiSeeder ora contiene un altro dato sui tamponi disponibili.

--- TicketYourTest/database/seeders/TamponiSeeder.php
+++ TicketYourTest/database/seeders/TamponiSeeder.php
@@ -14,9 +14,17 @@ class TamponiSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('tamponi')->insert([
-            'nome' => 'Tampone rapido',
-            'descrizione' => 'Il test rapido antigenico viene eseguito su un campione prelevato tramite tampone naso-faringeo. Esso ricerca alcune componenti proteiche del virus nei campioni prelevati mediante tampone. I tempi di risposta sono molto brevi, circa 5-30 minuti a seconda dello specifico kit utilizzato.'
-        ]);
+        $data = [
+            [
+                'nome' => 'Tampone rapido',
+                'descrizione' => 'Il test rapido antigenico viene eseguito su un campione prelevato tramite tampone naso-faringeo. Esso ricerca alcune componenti proteiche del virus nei campioni prelevati mediante tampone. I tempi di risposta sono molto brevi, circa 5-30 minuti a seconda dello specifico kit utilizzato.'
+            ],
+            [
+                'nome' => 'Tampone molecolare',
+                'descrizione' => 'Il principale e più affidabile strumento diagnostico è il cosiddetto tampone molecolare naso orofaringeo che consiste in un’indagine capace di rilevare il genoma (RNA) del virus SARS-Cov-2 nel campione biologico. L’esito di questo tampone si ottiene mediamente in tre/sei ore.'
+            ]
+
+        ];
+        DB::table('tamponi')->insert($data);
     }
 }
