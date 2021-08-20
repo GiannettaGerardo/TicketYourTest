2fecc8ee930fd8e16c13c2cb628368fd5167fcfc
CatignanoFrancesco <catignanof@gmail.com>
2021-08-20 21:30:42 +0200

    Aggiunto il middleware CittadinoMid

---
+++ TicketYourTest/app/Http/Middleware/CittadinoMid.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Attore;
+
+class CittadinoMid
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        if($request->session()->has('LoggedUser')) {
+            if($request->session()->get('Attore')===Attore::CITTADINO_PRIVATO) {
+                return $next($request);
+            }
+        }
+
+        return redirect('login');
+    }
+}
