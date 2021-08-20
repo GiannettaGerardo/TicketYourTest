85c2e4c01545fc7e88231ec0956a7250a33741c5
CatignanoFrancesco <catignanof@gmail.com>
2021-08-20 18:02:12 +0200

    Caso d'uso IF-LA.01 (convenziona) #11 Redmine issue 120137 #10
    Aggiunto il metodo "setCoordinateById" e modificato il metodo "convenzionaByEmail" in "convenzionaById".

--- TicketYourTest/app/Models/Laboratorio.php
+++ TicketYourTest/app/Models/Laboratorio.php
@@ -64,11 +64,46 @@ class Laboratorio extends Model
     }
 
     /**
-     * Effettua il convenzionamento di un laboratorio tramite la sua mail. Viene aggiornato il valore dell'attributo 'convenzionato'.
-     * @param $email
-     * @return int
+     * Imposta le coordinate del laboratorio a partire dal suo id.
+     * @param $id L'id del laboratorio.
+     * @param $coordinata_x La coordinata x.
+     * @param $coordinata_y La coordinata y.
+     * @return int Se l'update e' avvenuto con successo.
      */
-    static function convenzionaByEmail($email) {
-        return DB::table('laboratorio_analisi')->where('email', $email)->update(['convenzionato' => '1']);
+    static function setCoordinateById($id, $coordinata_x, $coordinata_y) {
+        return DB::table('laboratorio_analisi')
+            ->where('id', $id)
+            ->update([
+                'coordinata_x' => $coordinata_x,
+                'coordinata_y' => $coordinata_y
+            ]);
+    }
+
+    /**
+     * Effettua il convenzionamento di un laboratorio tramite il suo id. Viene aggiornato il valore dell'attributo 'convenzionato'.
+     * @param $id L'id del laboratorio.
+     * @return int Se l'update e' avvenuto con successo.
+     */
+    static function convenzionaById($id) {
+        return DB::table('laboratorio_analisi')->where('id', $id)->update(['convenzionato' => '1']);
+    }
+
+    /**
+     * Restituisce una collection contenente tutti i laboratori ancora non convenzionati.
+     * In particolare restituisce:
+     * - l'id
+     * - la partita iva
+     * - il nome
+     * - la provincia
+     * - la citta'
+     * - l'indirizzo
+     * - l'email
+     * @return \Illuminate\Support\Collection I laboratori o nulla
+     */
+    static function getLaboratoriNonConvenzionati() {
+        return DB::table('laboratorio_analisi')
+            ->select(['id', 'partita_iva', 'nome', 'provincia', 'citta', 'indirizzo', 'email'])
+            ->where('convenzionato', '=', 0)
+            ->get();
     }
 }
