3d1531a2ade07278b28fd78052bc3e508555a0d1
CatignanoFrancesco <catignanof@gmail.com>
2021-08-22 22:48:28 +0200

    Caso d'uso IF-DL.03 (aggiungiDipendentiAllaLista) #21 Redmine issue #120137
    Inserito il metodo "insertNewDipendente" nel model ListaDipendenti in modo da poter aggiungere un dipendente alla lista che non è registrato al sistema.

--- TicketYourTest/app/Models/ListaDipendenti.php
+++ TicketYourTest/app/Models/ListaDipendenti.php
@@ -34,4 +34,29 @@ class ListaDipendenti extends Model
     static function deleteCittadino($partita_iva, $codice_fiscale) {
         return DB::table('lista_dipendenti')->delete([$partita_iva, $codice_fiscale]);
     }
+
+    /**
+     * Inserisce un dipendente con tutte le sue informazioni nella lista dei dipendenti.
+     * Si presuppone che il dipendente sia gia' stato accettato nella lista.
+     * @param $partita_iva_datore La partita iva del datore di lavoro.
+     * @param $codice_fiscale Il codice fiscal del dipendente.
+     * @param $nome Il nome del dipendente.
+     * @param $cognome Il cognome del dipendente.
+     * @param $email L'email del dipendente.
+     * @param $citta_residenza La citta' di residenza del dipendente.
+     * @param $provincia_residenza La provincia di residenza del dipendente.
+     * @return bool L'esito dell'inserimento.
+     */
+    static function insertNewDipendente($partita_iva_datore, $codice_fiscale, $nome, $cognome, $email, $citta_residenza, $provincia_residenza) {
+        return DB::table('lista_dipendenti')->insert([
+            'partita_iva_datore' => $partita_iva_datore,
+            'codice_fiscale' => $codice_fiscale,
+            'nome' => $nome,
+            'cognome' => $cognome,
+            'email' => $email,
+            'citta_residenza' => $citta_residenza,
+            'provincia_residenza' => $provincia_residenza,
+            'accettato' => '1'
+        ]);
+    }
 }
