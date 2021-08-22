5b34534e92b8330441ade801ec80f3c94b663af4
CatignanoFrancesco <catignanof@gmail.com>
2021-08-22 23:09:10 +0200

    Caso d'uso IF-DL.03 (aggiungiDipendentiAllaLista) #21 Redmine issue #120137
    Aggiunto il metodo "inserisciDipendente" al controller ListaDipendentiController.

--- TicketYourTest/app/Http/Controllers/ListaDipendentiController.php
+++ TicketYourTest/app/Http/Controllers/ListaDipendentiController.php
@@ -37,9 +37,65 @@ class ListaDipendentiController extends Controller
         return back()->with('richiesta-avvenuta', 'La richiesta e\' avvenuta con successo!');
     }
 
+    /**
+     * Permette ad un cittadino di abbandonare la lista dipendenti.
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function abbandona(Request $request) {
         ListaDipendenti::deleteCittadino($request->input('iva'), $request->input('cf'));
 
         return back()->with('abbandono-success', 'Hai abbandonato la lista con successo!');
+    }
+
+    /**
+     * Permette ad un datore di lavoro di inserire un dipendente, anche se non e' registrato, alla lista dei dipendenti.
+     * Viene controllato, tramite l'indirizzo email, che non esista un cittadino privato gia' registrato.
+     * Se esiste, nella lista vengono inseriti solo i dati necessari (codice fiscale del dipendente e partita iva del datore). Altrimenti, vengono inserite
+     * tutte le informazioni necessarie per identificare un dipendente (come nome, cognome, citta' di residenza ecc...).
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function inserisciDipendente(Request $request) {
+        // Ottenimento del datore di lavoro dell'azienda
+        $id_datore = $request->session()->get('LoggedUser');
+        $datore = DatoreLavoro::getById($id_datore);
+
+        // Validazione del codice fiscale e dell'email
+        $request->validate([
+            'cf' => 'required|min:16|max:16',
+            'email' => 'required|email'
+        ]);
+
+        // Controllo dell'esistenza di un cittadino privato con quella email
+        // Se il cittadino esiste, viene aggiunto alla lista solo il codice fiscale, altrimenti, vengono aggiunte tutte le informazioni
+        $cittadino = CittadinoPrivato::getByEmail($request->input('email'));
+        if($cittadino) {
+            ListaDipendenti::insertNewCittadino($datore->partita_iva, $cittadino->codice_fiscale, 1);   // inserimento del cittadino
+        } else {
+            // Validazione degli altri dati
+            $request->validate([
+                'nome' => 'required|max:30',
+                'cognome' => 'required|max:30',
+                'citta_residenza' => 'required|max:50',
+                'provincia_residenza' => 'required|max:50'
+            ]);
+
+            // Ottenimento dei dati
+            $input = $request->all();
+
+            // Inserimento del dipendente alla lista
+            ListaDipendenti::insertNewDipendente(
+                $datore->partita_iva,
+                $input['cf'],
+                $input['nome'],
+                $input['cognome'],
+                $input['email'],
+                $input['citta_residenza'],
+                $input['provincia_residenza']
+            );
+        }
+
+        return back()->with('inserimento-success', 'Il dipendente e\' stato inserito con successo!');
     }
 }
