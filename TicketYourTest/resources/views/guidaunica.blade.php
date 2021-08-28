7a8a0603f0faae0e9801af1e1df9e640e056f023
FrancolinoFlavioDomenico <francolinoflavio@gmail.com>
2021-08-28 13:06:22 +0200

    modificato il tag link che includeva il css locale, ora viene usata la sintassi corretta richiesta da laravel

--- TicketYourTest/resources/views/guidaunica.blade.php
+++ TicketYourTest/resources/views/guidaunica.blade.php
@@ -11,7 +11,7 @@
     <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
-    <link rel="stylesheet" src="css/stile.css">
+    <link rel="stylesheet" href="{{ URL::asset('/css/stile.css') }}">
 
     <style>
 
