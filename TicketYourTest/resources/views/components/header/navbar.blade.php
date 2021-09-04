81eabb56747fc0b58f375c040272c9efc2336eff
FrancolinoFlavioDomenico <francolinoflavio@gmail.com>
2021-09-04 20:29:37 +0200

    rese dinamiche le voci presenti sulla navbar principale #8. Ora asseconda di quale utente sia loggato la navbar presenta le voci relative al dato utente

--- TicketYourTest/resources/views/components/header/navbar.blade.php
+++ TicketYourTest/resources/views/components/header/navbar.blade.php
@@ -1,8 +1,50 @@
 <nav id="mainNavBar" class="hiddenDisplay">
     <ul id="mainNavBar_items" class="rowP">
-        <li class="rowP"><a href="">Guida tamponi</a></li>
-        <li class="rowP"><a href="">Chi siamo</a></li>
-        <li class="rowP"><a href="">Login</a></li>
-        <li class="rowP"><a href="">Registrati</a></li>
+
+        @if (!Session::has('Attore'))
+
+            <li class="rowP"><a href="">Guida tamponi</a></li>
+            <li class="rowP"><a href="">Chi siamo</a></li>
+            <li class="rowP"><a href="">Login</a></li>
+            <li class="rowP"><a href="">Registrati</a></li>
+
+        @else
+
+            @if (Session::get('Attore') == 0){{--navbar items per l'amministratore--}}
+
+                <li class="rowP"><a href="">Richieste convenzionamento</a></li>
+                <li class="rowP"><a href="">Logout</a></li>
+
+            @elseif (Session::get('Attore') == 1){{--navbar items per il cittadino privato--}}
+
+                <li class="rowP"><a href="">Guida tamponi</a></li>
+                <li class="rowP"><a href="">Prenota tampone</a></li>
+                <li class="rowP"><a href="">Logout</a></li>
+
+            @elseif (Session::get('Attore') == 2){{--navbar items per il datore--}}
+
+                <li class="rowP"><a href="">Guida tamponi</a></li>
+                <li class="rowP"><a href="">Prenota tampone</a></li>
+                <li class="rowP"><a href="">Lista dipendenti</a></li>
+                <li class="rowP"><a href="">Logout</a></li>
+
+            @elseif (Session::get('Attore') == 3){{--navbar items per il medico--}}
+
+                <li class="rowP"><a href="">Guida tamponi</a></li>
+                <li class="rowP"><a href="">Prenota tampone</a></li>
+                <li class="rowP"><a href="">Lista dipendenti</a></li>
+                <li class="rowP"><a href="">Prenota tampone</a></li>
+                <li class="rowP"><a href="">Logout</a></li>
+
+            @elseif (Session::get('Attore') == 4){{--navbar items per il laboratorio--}}
+
+                <li class="rowP"><a href="">appuntamenti</a></li>
+                <li class="rowP"><a href="">Logout</a></li>
+
+            @endif
+
+        @endif
+
+
     </ul>
 </nav>
