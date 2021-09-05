ae99e196f43fd315d83bbd27d06f40e5ea76b7e7
GiannettaGerardo <ggp00519@protonmail.com>
2021-09-05 19:15:29 +0200

    Eliminata una chiamata ad un model che non serviva nel controller per le mappe

--- TicketYourTest/app/Http/Controllers/MappeController.php
+++ TicketYourTest/app/Http/Controllers/MappeController.php
@@ -14,21 +14,13 @@ use Illuminate\Http\Request;
 class MappeController extends Controller
 {
     /**
-     * Ritorna la vista contenente una mappa con i laboratori e alcune info sui tamponi proposti
+     * Ritorna la vista contenente una mappa con i laboratori
      * @param Request $request
      * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
      */
     public function getViewMappa(Request $request)
     {
         $laboratori = Laboratorio::getLaboratoriAttivi();
-        $tamponi_proposti_db = TamponiProposti::getTamponiPropostiLabAttivi();
-        $tamponi_proposti = array();
-        
-        foreach ($tamponi_proposti_db as $tupla) {
-            $tamponi_proposti[$tupla->id_laboratorio][] = array('id_tampone' => $tupla->id_tampone, 'costo' => $tupla->costo);
-        }
-
-        //dd($tamponi_proposti);
-        return view('laboratoriVicini', compact('laboratori', 'tamponi_proposti'));
+        return view('laboratoriVicini', compact('laboratori'));
     }
 }
