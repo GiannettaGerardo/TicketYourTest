3519e7ffc8dda611ab1024df8e13f588d2eaa558
GiannettaGerardo <ggp00519@protonmail.com>
2021-09-30 13:11:05 +0200

    Creare lo schema del Database #1
    Aggiunta la tabella de database prenotazioni

---
+++ TicketYourTest/database/migrations/2021_09_30_110029_create_prenotazioni.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePrenotazioni extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('prenotazioni', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('data_prenotazione');
+            $table->timestamp('data_tampone');
+            $table->unsignedBigInteger('id_tampone');
+            $table->string('cf_prenotante');
+            $table->unsignedBigInteger('id_laboratorio');
+
+            $table->foreign('id_tampone')->references('id')->on('tamponi');
+            $table->foreign('cf_prenotante')->references('codice_fiscale')->on('users');
+            $table->foreign('id_laboratorio')->references('id')->on('laboratorio_analisi');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('prenotazioni');
+    }
+}
