cb8a1eeef7bafdbed26a7c00852c99e03aa7a674
GiannettaGerardo <ggp00519@protonmail.com>
2021-10-01 00:50:26 +0200

    IF-UT.08 (visualizzaDisponibilitaTampone) #54
    Aggiunto il metodo per trovare la prima data disponibile per prenotare in un laboratorio, ma va completato

--- TicketYourTest/app/Http/Controllers/PrenotazioniController.php
+++ TicketYourTest/app/Http/Controllers/PrenotazioniController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\CalendarioDisponibilita;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use App\Models\User;
 
@@ -21,5 +23,67 @@ class PrenotazioniController extends Controller
         $utente = User::getById($request->session()->get('LoggedUser'));
 
         return view('form-prenotazione', compact('utente'));
+    }
+
+    /**
+     * Cerca la prima data disponibile per prenotare un tampone in un laboratorio.
+     * Se la prima data disponibile corrisponde con quella corrente, il limite di tempo
+     * per prenotare sarà di massimo 3 ore prima dell'orario di chiusura.
+     * @param Request $request
+     * @param $id_laboratorio // id del laboratorio selezionato dall'utente
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\View\Factory|\Illuminate\Contracts\View\View
+     */
+    public function visualizzaPrimaDataDisponibile(Request $request, $id_laboratorio)
+    {
+        $calendario = CalendarioDisponibilita::getCalendarioDisponibilitaByIdLaboratorio($id_laboratorio);
+        // creo un array di giorni con cui otterò il nome completo del giorno corrente
+        $array_giorni = ['lunedi', 'martedi', 'mercoledi', 'giovedi', 'venerdi', 'sabato', 'domenica'];
+        $array_giorni_numerici = [
+            'lunedi' => 0,
+            'martedi' => 1,
+            'mercoledi' => 2,
+            'giovedi' => 3,
+            'venerdi' => 4,
+            'sabato' => 5,
+            'domenica' => 6
+        ];
+        // ottengo il giorno della settimana corrente
+        $oggi = $array_giorni[Carbon::now()->dayOfWeek];
+        // ottengo l'ora corrente
+        $ora = intval(Carbon::now()->format('H'));
+
+        $giorni_calendario = [];
+        $orari = [];
+
+        foreach ($calendario as $c) {
+            $giorni_calendario[$c->giorno_settimana] = $array_giorni_numerici[$c->giorno_settimana];
+            $orari[$c->giorno_settimana] = ['oraApertura' => $c->oraApertura, 'oraChiusura' => $c->oraChiusura];
+
+            if ($c->giorno_settimana === $oggi) {
+                $ora_chiusura = intval(date('H', strtotime($c->oraChiusura)));
+
+                // sostituire true con un controllo sul numero di prenotazioni in quel giorno
+                // è possibile effettuare una prenotazione entro massimo 3 ore prima dell'ora di chiusura
+                if (($ora < ($ora_chiusura - 3)) and (true)) {
+                    // preparo le variabili di ritorno
+                    $giorno_return = $c->giorno_settimana;
+                    $ore_return = $orari[$c->giorno_settimana];
+                    return view('...', compact('giorno_return', 'ore_return'));
+                }
+            }
+        }
+        // cerco il primo giorno disponibile successivo al corrente
+        $oggi_giorno_numerico = $array_giorni_numerici[$oggi];
+        sort($giorni_calendario);
+
+        foreach ($giorni_calendario as $giorno) {
+            if ($giorno > $oggi_giorno_numerico) {
+                // preparo le variabili di ritorno
+                $giorno_return = $array_giorni[$giorno];
+                $ore_return = $orari[$array_giorni[$giorno]];
+                return view('...', compact('giorno_return', 'ore_return'));
+            }
+        }
+
     }
 }
