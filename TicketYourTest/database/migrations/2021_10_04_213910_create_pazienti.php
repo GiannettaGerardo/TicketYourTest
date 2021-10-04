72bd767d292f61938e90e76910124d98a9828b55
GiannettaGerardo <ggp00519@protonmail.com>
2021-10-04 23:54:39 +0200

    Creare lo schema del Database #1
    Aggiunta la tabella pazienti

---
+++ TicketYourTest/database/migrations/2021_10_04_213910_create_pazienti.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePazienti extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pazienti', function (Blueprint $table) {
+            $table->unsignedBigInteger('id_prenotazione');
+            $table->string('codice_fiscale');
+            $table->string('nome')->nullable();
+            $table->string('cognome')->nullable();
+            $table->string('email')->nullable();
+            $table->string('citta_residenza')->nullable();
+            $table->string('provincia_residenza')->nullable();
+            $table->binary('questionario_anamnesi')->nullable();
+            $table->boolean('esito_tampone')->nullable();
+
+            $table->primary(['id_prenotazione', 'codice_fiscale']);
+            $table->foreign('id_prenotazione')->references('id')->on('prenotazioni');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pazienti');
+    }
+}
