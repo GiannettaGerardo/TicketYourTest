65e44164fa27aa0132eb3840b77ec02fd325cc0f
fab0ba17 <f.bonsanto@studenti.uniba.it>
2021-10-12 10:28:18 +0200

    Caso d'uso IF-UT.07 (prenotazioneTampone) #57
    Effettuata una modifica inerente al nome della pagina del form di prenotazione
    tampone di una persona singola.

--- TicketYourTest/resources/views/formPrenotazioneTampone.blade.php
+++ TicketYourTest/resources/views/formPrenotazioneTampone.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title> Aggiungi Dipendente </title>
+    <title> Prenota tampone </title>
 
     <!-- Foglio di stile -->
     <link rel="stylesheet" href="{{ URL::asset('/css/stile.css') }}">
