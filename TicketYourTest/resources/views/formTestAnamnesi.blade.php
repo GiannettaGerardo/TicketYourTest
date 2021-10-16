f0cde2bf223575a7fb6b43f556003227f9ff3b8e
fab0ba17 <f.bonsanto@studenti.uniba.it>
2021-10-16 11:43:45 +0200

    Caso d'uso IF-UT.15 (compilaQuestionarioAnamnesi) #51 Redmine issue #123701 #47
    Effettuate alcune modifiche inerenti alla vista compilaQuestionarioAnamnesi.
    Le modifiche effettuate comprendono i diversi name e aggiunti i value
    per ogni domanda.
    
    La seguente vista deve essere ancora dinamicizzata e ulteriolmente modificata
    al fine di ottenere il risultato stabilito.

--- TicketYourTest/resources/views/formTestAnamnesi.blade.php
+++ TicketYourTest/resources/views/formTestAnamnesi.blade.php
@@ -49,51 +49,51 @@
                             </div>
                             <!--Prima domanda -->
                             <div class="mb-3 col-md-6">
-                                <label class="font-weight-bold">1- Indicare la motivazione per la quale si intende effettuare il tampone rapido antigenico: </label> <br>
+                                <label class="font-weight-bold">1- Indicare la motivazione per la quale si intende effettuare il tampone: </label> <br>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="motivazione" id="motivazione_1">
+                                    <input class="form-check-input" type="radio" name="motivazione" id="motivazione_1" value="sintomi">
                                     <label class="form-check-label" for="motivazione_1">
                                       Presenza di sintomi
                                     </label>
                                   </div>
                                   <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="motivazione" id="motivazione_2">
+                                    <input class="form-check-input" type="radio" name="motivazione" id="motivazione_2" value="contatto">
                                     <label class="form-check-label" for="motivazione_2">
                                       Contatto con positivi
                                     </label>
                                   </div>
                                   <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="motivazione" id="motivazione_3">
+                                    <input class="form-check-input" type="radio" name="motivazione" id="motivazione_3" value="controllo">
                                     <label class="form-check-label" for="motivazione_3">
                                       Controllo
                                     </label>
                                   </div>
                                   <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="motivazione" id="motivazione_4">
+                                    <input class="form-check-input" type="radio" name="motivazione" id="motivazione_4" value="accesso-struttura-sanitaria">
                                     <label class="form-check-label" for="motivazione_4">
                                       Accesso struttura sanitaria
                                     </label>
                                   </div>
                                   <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="motivazione" id="motivazione_5">
+                                    <input class="form-check-input" type="radio" name="motivazione" id="motivazione_5" value="viaggi-trasferta">
                                     <label class="form-check-label" for="motivazione_5">
                                       Viaggi e trasferte
                                     </label>
                                   </div>
                                   <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="motivazione" id="motivazione_6">
+                                    <input class="form-check-input" type="radio" name="motivazione" id="motivazione_6" value="lavoro">
                                     <label class="form-check-label" for="motivazione_6">
                                       Attività lavorativa
                                     </label>
                                   </div>
                                   <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="motivazione" id="motivazione_7">
+                                    <input class="form-check-input" type="radio" name="motivazione" id="motivazione_7" value="sport">
                                     <label class="form-check-label" for="motivazione_7">
                                       Attività sportiva
                                     </label>
                                   </div>
                                   <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="motivazione" id="motivazione_8">
+                                    <input class="form-check-input" type="radio" name="motivazione" id="motivazione_8" value="scuola">
                                     <label class="form-check-label" for="motivazione_8">
                                       Attività scolastica
                                     </label>
@@ -103,14 +103,14 @@
                             <div class="mb-3 col-md-6">
                                 <label class="font-weight-bold">2- In questo periodo sta lavorando? </label>
                                     <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="febbre" id="isfebbre">
-                                        <label class="form-check-label" for="isfebbre">
+                                        <input class="form-check-input" type="radio" name="lavoro" id="islavoro" value="si">
+                                        <label class="form-check-label" for="islavoro">
                                         Si
                                         </label>
                                     </div>
                                     <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="febbre" id="notfebbre">
-                                        <label class="form-check-label" for="notfebbre">
+                                        <input class="form-check-input" type="radio" name="lavoro" id="notlavoro" value="no">
+                                        <label class="form-check-label" for="notlavoro">
                                         No
                                         </label>
                                     </div>
@@ -122,14 +122,14 @@
                                     al Covid-19? 
                                 </label>
                                     <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="allergie" id="isallergia">
-                                        <label class="form-check-label" for="isallergia">
+                                        <input class="form-check-input" type="radio" name="contatto" id="iscontatto" value="si">
+                                        <label class="form-check-label" for="iscontatto">
                                         Si
                                         </label>
                                     </div>
                                     <div class="form-check">
-                                        <input class="form-check-input" type="radio" name="allergie" id="notallergia">
-                                        <label class="form-check-label" for="notallergia">
+                                        <input class="form-check-input" type="radio" name="contatto" id="notcontatto" value="no">
+                                        <label class="form-check-label" for="notcontatto">
                                         No
                                         </label>
                                     </div>
@@ -141,14 +141,14 @@
                                     giorni dall’ultimo contatto?
                                 </label>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="reazioneAllergica" id="isReazioneGrave">
-                                    <label class="form-check-label" for="isReazioneGrave">
+                                    <input class="form-check-input" type="radio" name="quindici-giorni-dopo-contatto" id="giorni-dopo-contatto" value="si">
+                                    <label class="form-check-label" for="giorni-dopo-contatto">
                                     Si
                                     </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="reazioneAllergica" id="notReazioneGrave">
-                                    <label class="form-check-label" for="notReazioneGrave">
+                                    <input class="form-check-input" type="radio" name="quindici-giorni-dopo-contatto" id="not-giorni-dopo-contatto" value="no">
+                                    <label class="form-check-label" for="not-giorni-dopo-contatto">
                                     No
                                     </label>
                                 </div>
@@ -161,14 +161,14 @@
                                     
                                 </label>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="malattieVarie" id="isMalattieVarie">
-                                    <label class="form-check-label" for="isMalattieVarie">
+                                    <input class="form-check-input" type="radio" name="tampone-fatto" id="tampone-fatto" value="si">
+                                    <label class="form-check-label" for="tampone-fatto">
                                     Si
                                     </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="malattieVarie" id="notMalattieVarie">
-                                    <label class="form-check-label" for="notMalattieVarie">
+                                    <input class="form-check-input" type="radio" name="tampone-fatto" id="not-tampone-fatto" value="no">
+                                    <label class="form-check-label" for="not-tampone-fatto">
                                     No
                                     </label>
                                 </div>
@@ -180,14 +180,14 @@
                                     (quarantena)?
                                 </label>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="compromissioniVarie" id="isCompromissioneVarie">
-                                    <label class="form-check-label" for="isCompromissioneVarie">
+                                    <input class="form-check-input" type="radio" name="isolamento" id="is-isolamento" value="si">
+                                    <label class="form-check-label" for="is-isolamento">
                                     Si
                                     </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="compromissioniVarie" id="notCompromissioneVarie">
-                                    <label class="form-check-label" for="notCompromissioneVarie">
+                                    <input class="form-check-input" type="radio" name="isolamento" id="not-isolamento" value="no">
+                                    <label class="form-check-label" for="not-isolamento">
                                     No
                                     </label>
                                 </div>
@@ -199,14 +199,14 @@
                                     da Covid-19?                                    
                                 </label>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="infoConvulsione" id="isConvulsione">
-                                    <label class="form-check-label" for="isConvulsione">
+                                    <input class="form-check-input" type="radio" name="info-contagio-covid" id="is-contagiato-covid" value="si">
+                                    <label class="form-check-label" for="is-contagiato-covid">
                                     Si
                                     </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="radio" name="infoConvulsione" id="notConvulsione">
-                                    <label class="form-check-label" for="notConvulsione">
+                                    <input class="form-check-input" type="radio" name="info-contagio-covid" id="not-contagiato-covid" value="no">
+                                    <label class="form-check-label" for="not-contagiato-covid">
                                     No
                                     </label>
                                 </div>
@@ -218,49 +218,49 @@
                                     sintomi?                                   
                                 </label>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="infoSintomi" id="febbre">
+                                    <input class="form-check-input" type="checkbox" name="sintomi[]" id="febbre" value="si-febbre">
                                     <label class="form-check-label" for="febbre">
                                     Febbre
                                     </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="infoSintomi" id="tosse">
+                                    <input class="form-check-input" type="checkbox" name="sintomi[]" id="tosse" value="si-tosse">
                                     <label class="form-check-label" for="tosse">
                                     Tosse
                                     </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="infoSintomi" id="difficolta_respiratorie">
+                                    <input class="form-check-input" type="checkbox" name="sintomi[]" id="difficolta_respiratorie" value="si-difficoltà-respiratorie">
                                     <label class="form-check-label" for="difficolta_respiratorie">
                                     Difficoltà respiratorie
                                     </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="infoSintomi" id="raffreddore">
+                                    <input class="form-check-input" type="checkbox" name="sintomi[]" id="raffreddore" value="si-raffreddore">
                                     <label class="form-check-label" for="raffreddore">
                                     Raffreddore
                                     </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="infoSintomi" id="malDiGola">
+                                    <input class="form-check-input" type="checkbox" name="sintomi[]" id="malDiGola" value="si-malDiGola">
                                     <label class="form-check-label" for="malDiGola">
                                     Mal di gola
                                     </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="infoSintomi" id="mancanzaGusto">
+                                    <input class="form-check-input" type="checkbox" name="sintomi[]" id="mancanzaGusto" value="si-alterazione-gusto">
                                     <label class="form-check-label" for="mancanzaGusto">
                                     Alterazione del gusto
                                     </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="infoSintomi" id="doloriMuscolari">
+                                    <input class="form-check-input" type="checkbox" name="sintomi[]" id="doloriMuscolari" value="si-dolori-muscolari">
                                     <label class="form-check-label" for="doloriMuscolari">
                                     Dolori muscolari
                                     </label>
                                 </div>
                                 <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="infoSintomi" id="cefalea">
+                                    <input class="form-check-input" type="checkbox" name="sintomi[]" id="cefalea" value="si-cefalea">
                                     <label class="form-check-label" for="cefalea">
                                     Cefalea
                                     </label>
