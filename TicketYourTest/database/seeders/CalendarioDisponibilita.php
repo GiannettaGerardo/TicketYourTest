1b4f5aa53239730c9827cb152b95156fa55c2bf9
GiannettaGerardo <ggp00519@protonmail.com>
2021-11-12 01:15:05 +0100

    Aggiunti nuovi seeder laboratori

--- TicketYourTest/database/seeders/CalendarioDisponibilita.php
+++ TicketYourTest/database/seeders/CalendarioDisponibilita.php
@@ -19,12 +19,54 @@ class CalendarioDisponibilita extends Seeder
         $ore_8_am = date('H:i:s', strtotime('08:00:00'));
         $ore_21_am = date('H:i:s', strtotime('21:00:00'));
         $ore_15_am = date('H:i:s', strtotime('15:00:00'));
+        $ore_18_am = date('H:i:s', strtotime('18:00:00'));
         $data = [
             [ 'giorno_settimana' => 'lunedi', 'id_laboratorio' => 1, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_21_am ],
             [ 'giorno_settimana' => 'martedi', 'id_laboratorio' => 1, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_21_am ],
             [ 'giorno_settimana' => 'giovedi', 'id_laboratorio' => 1, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_21_am ],
             [ 'giorno_settimana' => 'venerdi', 'id_laboratorio' => 1, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_21_am ],
             [ 'giorno_settimana' => 'sabato', 'id_laboratorio' => 1, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_15_am ],
+
+            [ 'giorno_settimana' => 'lunedi', 'id_laboratorio' => 6, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_18_am ],
+            [ 'giorno_settimana' => 'martedi', 'id_laboratorio' => 6, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_21_am ],
+            [ 'giorno_settimana' => 'giovedi', 'id_laboratorio' => 6, 'oraApertura' => $ore_15_am, 'oraChiusura' => $ore_21_am ],
+            [ 'giorno_settimana' => 'venerdi', 'id_laboratorio' => 6, 'oraApertura' => $ore_15_am, 'oraChiusura' => $ore_18_am ],
+
+            [ 'giorno_settimana' => 'martedi', 'id_laboratorio' => 2, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_18_am ],
+            [ 'giorno_settimana' => 'venerdi', 'id_laboratorio' => 2, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_18_am ],
+            [ 'giorno_settimana' => 'sabato', 'id_laboratorio' => 2, 'oraApertura' => $ore_15_am, 'oraChiusura' => $ore_15_am ],
+
+            [ 'giorno_settimana' => 'lunedi', 'id_laboratorio' => 7, 'oraApertura' => $ore_18_am, 'oraChiusura' => $ore_21_am ],
+            [ 'giorno_settimana' => 'martedi', 'id_laboratorio' => 7, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_21_am ],
+            [ 'giorno_settimana' => 'giovedi', 'id_laboratorio' => 7, 'oraApertura' => $ore_15_am, 'oraChiusura' => $ore_21_am ],
+            [ 'giorno_settimana' => 'venerdi', 'id_laboratorio' => 7, 'oraApertura' => $ore_18_am, 'oraChiusura' => $ore_21_am ],
+            [ 'giorno_settimana' => 'sabato', 'id_laboratorio' => 7, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_15_am ],
+
+            [ 'giorno_settimana' => 'martedi', 'id_laboratorio' => 8, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_21_am ],
+            [ 'giorno_settimana' => 'venerdi', 'id_laboratorio' => 8, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_18_am ],
+            [ 'giorno_settimana' => 'sabato', 'id_laboratorio' => 8, 'oraApertura' => $ore_15_am, 'oraChiusura' => $ore_15_am ],
+
+            [ 'giorno_settimana' => 'lunedi', 'id_laboratorio' => 9, 'oraApertura' => $ore_18_am, 'oraChiusura' => $ore_21_am ],
+            [ 'giorno_settimana' => 'martedi', 'id_laboratorio' => 9, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_21_am ],
+            [ 'giorno_settimana' => 'giovedi', 'id_laboratorio' => 9, 'oraApertura' => $ore_15_am, 'oraChiusura' => $ore_21_am ],
+            [ 'giorno_settimana' => 'venerdi', 'id_laboratorio' => 9, 'oraApertura' => $ore_18_am, 'oraChiusura' => $ore_21_am ],
+
+            [ 'giorno_settimana' => 'lunedi', 'id_laboratorio' => 10, 'oraApertura' => $ore_18_am, 'oraChiusura' => $ore_21_am ],
+            [ 'giorno_settimana' => 'martedi', 'id_laboratorio' => 10, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_21_am ],
+            [ 'giorno_settimana' => 'giovedi', 'id_laboratorio' => 10, 'oraApertura' => $ore_15_am, 'oraChiusura' => $ore_21_am ],
+            [ 'giorno_settimana' => 'venerdi', 'id_laboratorio' => 10, 'oraApertura' => $ore_18_am, 'oraChiusura' => $ore_21_am ],
+
+            [ 'giorno_settimana' => 'martedi', 'id_laboratorio' => 11, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_21_am ],
+            [ 'giorno_settimana' => 'venerdi', 'id_laboratorio' => 11, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_18_am ],
+            [ 'giorno_settimana' => 'sabato', 'id_laboratorio' => 11, 'oraApertura' => $ore_15_am, 'oraChiusura' => $ore_15_am ],
+
+            [ 'giorno_settimana' => 'lunedi', 'id_laboratorio' => 12, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_18_am ],
+            [ 'giorno_settimana' => 'martedi', 'id_laboratorio' => 12, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_21_am ],
+            [ 'giorno_settimana' => 'giovedi', 'id_laboratorio' => 12, 'oraApertura' => $ore_15_am, 'oraChiusura' => $ore_21_am ],
+            [ 'giorno_settimana' => 'venerdi', 'id_laboratorio' => 12, 'oraApertura' => $ore_15_am, 'oraChiusura' => $ore_18_am ],
+
+            [ 'giorno_settimana' => 'martedi', 'id_laboratorio' => 13, 'oraApertura' => $ore_15_am, 'oraChiusura' => $ore_21_am ],
+            [ 'giorno_settimana' => 'venerdi', 'id_laboratorio' => 13, 'oraApertura' => $ore_8_am, 'oraChiusura' => $ore_18_am ],
         ];
         DB::table($tableName)->insert($data);
     }
--- TicketYourTest/database/seeders/TamponiPropostiSeeder.php
+++ TicketYourTest/database/seeders/TamponiPropostiSeeder.php
@@ -30,6 +30,76 @@ class TamponiPropostiSeeder extends Seeder
                 'id_laboratorio' => 5,
                 'id_tampone' => 1,
                 'costo' => 9.99
+            ],
+            [
+                'id_laboratorio' => 6,
+                'id_tampone' => 1,
+                'costo' => 9.99
+            ],
+            [
+                'id_laboratorio' => 2,
+                'id_tampone' => 2,
+                'costo' => 14.99
+            ],
+            [
+                'id_laboratorio' => 7,
+                'id_tampone' => 1,
+                'costo' => 9.99
+            ],
+            [
+                'id_laboratorio' => 7,
+                'id_tampone' => 2,
+                'costo' => 12.99
+            ],
+            [
+                'id_laboratorio' => 8,
+                'id_tampone' => 1,
+                'costo' => 10.99
+            ],
+            [
+                'id_laboratorio' => 9,
+                'id_tampone' => 1,
+                'costo' => 9.99
+            ],
+            [
+                'id_laboratorio' => 9,
+                'id_tampone' => 2,
+                'costo' => 13.99
+            ],
+            [
+                'id_laboratorio' => 10,
+                'id_tampone' => 1,
+                'costo' => 10.99
+            ],
+            [
+                'id_laboratorio' => 10,
+                'id_tampone' => 2,
+                'costo' => 15.99
+            ],
+            [
+                'id_laboratorio' => 11,
+                'id_tampone' => 1,
+                'costo' => 10.99
+            ],
+            [
+                'id_laboratorio' => 11,
+                'id_tampone' => 2,
+                'costo' => 14.99
+            ],
+            [
+                'id_laboratorio' => 12,
+                'id_tampone' => 1,
+                'costo' => 8.50
+            ],
+            [
+                'id_laboratorio' => 12,
+                'id_tampone' => 2,
+                'costo' => 14.50
+            ],
+            [
+                'id_laboratorio' => 13,
+                'id_tampone' => 2,
+                'costo' => 15.00
             ]
         ];
         DB::table($tableName)->insert($data);
