00dd968604f61ed2143b9453ae21500e4a9cf7d4
CatignanoFrancesco <catignanof@gmail.com>
2021-11-16 20:02:17 +0100

    Risolto problema sulla refresh dei seeder

--- TicketYourTest/database/seeders/QuestionarioAnamnesiSeeder.php
+++ TicketYourTest/database/seeders/QuestionarioAnamnesiSeeder.php
@@ -22,7 +22,21 @@ class QuestionarioAnamnesiSeeder extends Seeder
                 'cf_paziente' => 'CTGFNC00B10E716C',
                 'token' => Str::uuid(),
                 'token_scaduto' => 0,
-                'motivazione' => null
+                'motivazione' => null,
+                'lavoro' => 0,
+                'contatto' => 0,
+                'quindici-giorni-dopo-contatto' => 0,
+                'tampone-fatto' => 0,
+                'isolamento' => 0,
+                'contagiato' => 0,
+                'febbre' => 0,
+                'tosse' => 0,
+                'difficolta-respiratorie' => 0,
+                'raffreddore' => 0,
+                'mal-di-gola' => 0,
+                'mancanza-gusto' => 0,
+                'dolori-muscolari' => 0,
+                'cefalea' => 0
             ],
 
             [
@@ -52,7 +66,21 @@ class QuestionarioAnamnesiSeeder extends Seeder
                 'cf_paziente' => 'MSSVLR07R67I930S',
                 'token' => Str::uuid(),
                 'token_scaduto' => 0,
-                'motivazione' => null
+                'motivazione' => null,
+                'lavoro' => 0,
+                'contatto' => 0,
+                'quindici-giorni-dopo-contatto' => 0,
+                'tampone-fatto' => 0,
+                'isolamento' => 0,
+                'contagiato' => 0,
+                'febbre' => 0,
+                'tosse' => 0,
+                'difficolta-respiratorie' => 0,
+                'raffreddore' => 0,
+                'mal-di-gola' => 0,
+                'mancanza-gusto' => 0,
+                'dolori-muscolari' => 0,
+                'cefalea' => 0
             ]
         ];
 
