7b248c9ecd9c80a182d5e43859a0d9ce147ec3b4
CatignanoFrancesco <catignanof@gmail.com>
2021-11-26 20:18:24 +0100

    Aggiunta la data del referto
    Caso d'uso IF-ASL.03 (visualizzaNumeroTamponiEffettuati) #83
    
    Redmine Issue 136671 #80

--- TicketYourTest/database/migrations/2021_11_25_191517_create_referti.php
+++ TicketYourTest/database/migrations/2021_11_25_191517_create_referti.php
@@ -19,6 +19,7 @@ class CreateReferti extends Migration
             $table->string('cf_paziente', 16);
             $table->enum('esito_tampone', ['positivo', 'negativo', 'indeterminato'])->nullable();
             $table->float('quantita')->nullable();
+            $table->timestamp('data_referto')->nullable();
 
             $table->foreign(['id_prenotazione', 'cf_paziente'])->references(['id_prenotazione', 'codice_fiscale'])->on('pazienti')
                 ->onDelete('cascade')
--- TicketYourTest/database/seeders/RefertiSeeder.php
+++ TicketYourTest/database/seeders/RefertiSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Carbon\Carbon;
 
 class RefertiSeeder extends Seeder
 {
@@ -19,28 +20,32 @@ class RefertiSeeder extends Seeder
                 'id_prenotazione' => 1,
                 'cf_paziente' => 'CTGFNC00B10E716C',
                 'esito_tampone' => null,
-                'quantita' => null
+                'quantita' => null,
+                'data_referto' => null
             ],
 
             [
                 'id_prenotazione' => 2,
                 'cf_paziente' => 'BSCBCM54E50G372U',
                 'esito_tampone' => null,
-                'quantita' => null
+                'quantita' => null,
+                'data_referto' => null
             ],
 
             [
                 'id_prenotazione' => 3,
                 'cf_paziente' => 'MSSVLR07R67I930S',
                 'esito_tampone' => 'positivo',
-                'quantita' => 15.3
+                'quantita' => 15.3,
+                'data_referto' => Carbon::now()->addDays(4)->format('Y-m-d')
             ],
 
             [
                 'id_prenotazione' => 4,
                 'cf_paziente' => 'VWLPBV95C02F646R',
                 'esito_tampone' => 'positivo',
-                'quantita' => 17.2
+                'quantita' => 17.2,
+                'data_referto' => Carbon::createFromFormat('Y-m-d', '2021-11-16')->addDay()->format('Y-m-d')
             ]
         ];
 
