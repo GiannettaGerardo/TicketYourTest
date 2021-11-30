e6564fe9c49c07fb3f1c6529812163171f708fe3
crime <crime@localhost.localdomain>
2021-11-30 23:00:30 +0100

    effettuate piccole modifiche grafiche #92 #93.

--- TicketYourTest/resources/views/storicoTamponi.blade.php
+++ TicketYourTest/resources/views/storicoTamponi.blade.php
@@ -33,12 +33,12 @@
 
         @endif
 
-
+        <hr>
         
         @if ((Session::get('Attore') == 2) && (!$prenotazioni_dipendenti->isEmpty()))
 
-            <hr>
             <x-storico-prenotazioni.tabella-storico-per-terzi :prenotazioni="$prenotazioni_dipendenti" />
+            <hr>
 
         @endif
     @endif
