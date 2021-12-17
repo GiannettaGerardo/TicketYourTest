ff013f8018304ef1bec9773bd42e6f0acac00d9f
Francolino Flavio Domenico <francolinoflavio@gmail.com>
2021-12-18 00:40:30 +0100

    implementata pagina che viene convertita in pdf per visualizzare il referto di un tampone

--- TicketYourTest/resources/views/referto.blade.php
+++ TicketYourTest/resources/views/referto.blade.php
@@ -1,12 +1,47 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="it">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>referto</title>
+
+    <link rel="stylesheet" href="{{ URL::asset('/css/stile.css') }}">
 </head>
+
 <body>
-    
+
+
+    <table class="refertoTable">
+        <thead>
+            <tr>
+                <th>Intestazione</th>
+            </tr>
+        </thead>
+
+        <tbody>
+
+            <tr>
+                <td><b>codice fiscale</b><br>{{$referto->cf_paziente}}</td>
+                <td><b>nome e cognome</b><br>{{$referto->nome_paziente}} {{$referto->cognome_paziente}}</td>
+            </tr>
+
+            <tr>
+                <td><b>data tampone</b><br>{{$referto->data_tampone}}</td>
+                <td><b>data referto</b><br>{{$referto->data_referto}}</td>
+            </tr>
+
+            <tr>
+                <td><b>esito</b><br>{{$referto->esito_tampone}}</td>
+                @if($referto->esito_tampone !== "negativo")
+                <td><b>quantita</b><br>{{$referto->quantita}}</td>
+                @endif
+            </tr>
+
+        </tbody>
+    </table>
+
 </body>
+
 </html>
