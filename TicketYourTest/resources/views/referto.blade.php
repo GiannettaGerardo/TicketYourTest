381789ddd08b7c8d3903ace469441fd4f2d3f763
Flavio Francolino <78420636+FrancolinoFlavioDomenico@users.noreply.github.com>
2021-12-19 02:55:40 +0100

    corretta commit precedente
    la commit precedente a questa ha caricato una versione errata del medesimo file per cui ora è stato aggiunto il corretto codice che sarebbe dovuto essere caricato con la commit precedente

--- TicketYourTest/resources/views/referto.blade.php
+++ TicketYourTest/resources/views/referto.blade.php
@@ -12,6 +12,64 @@
 
 <body>
 
+<table style="width: 100%">
+
+        <caption style="text-align: left">
+            <h2>
+                TicketYourTest<br>
+                <small>referto</small>
+            </h2>
+        </caption>
+
+        <tbody style="font-size: 1.2em">
+
+            <tr>
+
+                <td style="width=50%">
+                    <label> <strong>Codice Fiscale:</strong> </label> <br>
+                    <span>{{ $referto->cf_paziente}}</span>
+                </td>
+
+                <td style="width=50%">
+                    <label> <strong>Nome e cognome:</strong> </label> <br>
+                    <span>{{ $referto->nome_paziente}} {{ $referto->cognome_paziente}}</span>
+                </td>
+
+            </tr>
+
+            <tr>
+
+                <td style="width=50%">
+                    <label> <strong>Data tampone:</strong> </label> <br>
+                    <span>{{ $referto->data_tampone}}</span>
+                </td>
+
+                <td style="width=50%">
+                    <label> <strong>Data referto:</strong> </label> <br>
+                    <span>{{ $referto->data_referto}}</span>
+                </td>
+
+            </tr>
+
+            <tr>
+
+                <td style="width=50%">
+                    <label> <strong>Desito tampone:</strong> </label> <br>
+                    <span>{{ $referto->esito_tampone}}</span>
+                </td>
+
+                @if ($referto->esito_tampone !== "negativo")
+                    <td style="width=50%">
+                        <label> <strong>Carica virale:</strong> </label> <br>
+                        <span>{{ $referto->quantita}}</span>
+                    </td>
+                @endif
+
+            </tr>
+           
+        </tbody>
+
+    </table>
 
     
 </body>
