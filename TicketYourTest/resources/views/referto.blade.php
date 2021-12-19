bb99e109c237f9357e4b3e8a9c754cbc35a56e88
crime <crime@localhost.localdomain>
2021-12-19 02:48:41 +0100

    definito stile css per la pagina che visualizzare il referto di un tampone in pdf

--- TicketYourTest/resources/views/referto.blade.php
+++ TicketYourTest/resources/views/referto.blade.php
@@ -13,35 +13,7 @@
 <body>
 
 
-    <table class="refertoTable">
-        <thead>
-            <tr>
-                <th>Intestazione</th>
-            </tr>
-        </thead>
-
-        <tbody>
-
-            <tr>
-                <td><b>codice fiscale</b><br>{{$referto->cf_paziente}}</td>
-                <td><b>nome e cognome</b><br>{{$referto->nome_paziente}} {{$referto->cognome_paziente}}</td>
-            </tr>
-
-            <tr>
-                <td><b>data tampone</b><br>{{$referto->data_tampone}}</td>
-                <td><b>data referto</b><br>{{$referto->data_referto}}</td>
-            </tr>
-
-            <tr>
-                <td><b>esito</b><br>{{$referto->esito_tampone}}</td>
-                @if($referto->esito_tampone !== "negativo")
-                <td><b>quantita</b><br>{{$referto->quantita}}</td>
-                @endif
-            </tr>
-
-        </tbody>
-    </table>
-
+    
 </body>
 
 </html>
