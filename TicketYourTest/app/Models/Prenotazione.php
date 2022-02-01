682b5a19e4f9ddfbb8a063e3e6e0cae6f3f17563
CatignanoFrancesco <catignanof@gmail.com>
2022-02-01 17:45:40 +0100

    fix L'apertura del referto non funziona correttamente #114
    Sistemate le query

--- TicketYourTest/app/Models/Prenotazione.php
+++ TicketYourTest/app/Models/Prenotazione.php
@@ -317,7 +317,8 @@ class Prenotazione extends Model
     {
         return self::getJoinPazientiTamponiLaboratorio()
             ->where('pazienti.codice_fiscale', $codice_fiscale)
-            ->where('prenotazioni.data_tampone', '<', Carbon::now()->format('Y-m-d'))
+            ->where('prenotazioni.data_tampone', '<=', Carbon::now()->format('Y-m-d'))
+            ->whereNotNull('referti.esito_tampone')
             ->select(
                 'prenotazioni.data_tampone as data_tampone',
                 'tamponi.nome as tipo_tampone',
@@ -339,7 +340,8 @@ class Prenotazione extends Model
             ->join('users', 'users.codice_fiscale', '=', 'prenotazioni.cf_prenotante')
             ->where('prenotazioni.cf_prenotante', $codice_f_prenotante)
             ->whereColumn('prenotazioni.cf_prenotante', '<>', 'pazienti.codice_fiscale')
-            ->where('prenotazioni.data_tampone', '<', Carbon::now()->format('Y-m-d'))
+            ->where('prenotazioni.data_tampone', '<=', Carbon::now()->format('Y-m-d'))
+            ->whereNotNull('referti.esito_tampone')
             ->selectRaw(
                 'prenotazioni.id as id_prenotazione, '.
                 'pazienti.codice_fiscale as cf_terzo, '.
